566d4496149389d8c713063eb4f32aa2319dcef2
PotentialParadox <dtracy@ufl.edu>
2015-07-02 00:54:12 -0400

    Added yet more of the old code

--- src/numFunc.h
+++ src/numFunc.h
@@ -7,12 +7,13 @@
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 template<class T>
 class numFunc {
 public:
     // Constructor that requires a single number
-    numFunc(T &number) {
+    numFunc(T number) {
         setNumber(number);
     }
 
@@ -35,6 +36,43 @@ public:
         }
         return fib;
     }
+
+    // The basic prime tester
+    bool isPrime(){
+        bool condition = 1;
+        T i = 2;
+        while ( condition && (i <= (T)pow(N, 0.5)))
+        {
+            if (!(N%i))
+            {
+                condition=0;
+            }
+            i++;
+        }
+        return condition;
+    }
+
+    std::vector<T> listPrimeFactors() {
+        std::vector<T> primeList;
+        T maxVal = N;
+        for (long long int i = 2; i <= maxVal; ++i)
+        {
+            if (!(N % i) && isPrime())
+            {
+                while (!(maxVal % i))
+                {
+                    maxVal = maxVal / i;
+                    primeList.push_back(i);
+                }
+            }
+        }
+        if (primeList.empty())
+        {
+            primeList.push_back(N);
+        }
+        return primeList;
+    }
+
 
 private:
     T N;
